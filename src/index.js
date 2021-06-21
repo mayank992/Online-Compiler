ef6eb68c60d6e502b686776482976371d06e1af8
mayank992 <mayankjindal992@gmail.com>
2021-06-21 17:34:22 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const submissions = require('./submissions.route.js');
 
 const app = express();
 const port = process.env.PORT;
-const publicDirPath = path.join(__dirname, '../client');
+const publicDirPath = path.join(__dirname, '../client/build');
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicDirPath));
 
-app.use('/run', submissions);
-
 app.get('/', (req, res) => {
-  res.status(200).sendFile('../client/index.html');
+  res.status(200).sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
+app.use('/run', submissions);
 
 app.listen(port, () => {
   console.log(`Server is running on PORT: ${port}...`);
